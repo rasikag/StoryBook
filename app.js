4e7f1caba9c07c17d35ea95279868cf43d0cd504
rasikag <rasikagayan90@gmail.com>
2018-02-06 23:17:07 +0800

    default route added

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.get('/' , (req, res) => {
+    res.send('Get');
+});
+
 const port = process.env.PORT || 8081;
 
 app.listen(port, () =>{
-    console.log(`Server started on port: ${port}`);
+    console.log(`Server started on port: ${port }`);
 });
