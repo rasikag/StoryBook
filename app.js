b03dad6d0ee4c0a2d38d8e9d6537b6c89d189373
rasikag <rasikagayan90@gmail.com>
2018-02-07 01:26:51 +0800

    added auth route and connected it

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+// Load routes 
+const auth = require('./routes/auth');
 const app = express();
 
 app.get('/' , (req, res) => {
     res.send('Get');
 });
+
+// use auth route
+app.use('/auth', auth);
 //588847944644-5ji47ghqq4vklr0331qf4rspmmmo2hr2.apps.googleusercontent.com
 //Ky5oS1-1yHTkyTym17qwM5gc
 const port = process.env.PORT || 8081;
---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/google', (req, res) => {
+    res.send('auth');
+});
+
+
+module.exports = router;
