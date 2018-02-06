baa15b56bdc99609691cdc8391896d89db264cf5
rasikag <rasikagayan90@gmail.com>
2018-02-07 00:19:18 +0800

    keys added

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = express();
 app.get('/' , (req, res) => {
     res.send('Get');
 });
-
+//588847944644-5ji47ghqq4vklr0331qf4rspmmmo2hr2.apps.googleusercontent.com
+//Ky5oS1-1yHTkyTym17qwM5gc
 const port = process.env.PORT || 8081;
 
 app.listen(port, () =>{
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI : 'mongodb://rasikag:rasikag@ds125388.mlab.com:25388/rasikag-storybook-dev',
+    googleClientID : '588847944644-5ji47ghqq4vklr0331qf4rspmmmo2hr2.apps.googleusercontent.com',
+    googleClientSecret : 'Ky5oS1-1yHTkyTym17qwM5gc'
+}
