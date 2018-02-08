70f25767f2af4fb9241284cd3ed856f694a8fba1
rasikag <rasikagayan90@gmail.com>
2018-02-09 01:15:15 +0800

    set user

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,9 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('./keys');
 
+// Load user model 
+const User = mongoose.model('users');
+
 module.exports = function(passport){
     passport.use(
         new GoogleStrategy({
@@ -19,7 +22,20 @@ module.exports = function(passport){
                 lastName : profile.name.familyName,
                 email : profile.emails[0].value,
                 image :  image
-            }
+            };
+
+            User.findOne({ 
+                googleID : profile.id
+            }).then(user => {
+                if(user){
+                    done(null, user);
+                } else{
+                    // Create new user 
+                    new User(newUser)
+                        .save()
+                        .then(user => done(null, user));
+                }
+            });
         })
     );
 }  
